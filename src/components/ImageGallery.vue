54491cd8727b006c67b28399b11d84031b93dc08
Tsvetelina Stoykova <cveti.stoikova@gmail.com>
2020-06-22 18:31:11 +0300

    gallery

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,69 @@
+<template>
+
+      
+    <div class="ImageGallery">
+        <div v-for="g in gallery" class="gallery" :key="g.imgSrc">
+            <a >
+                <img :src="g.imgSrc" :alt="g.imgAlt">
+            </a>
+            <div class="desc">
+                <template v-if="g.showDescription">
+                    {{ g.description }}
+                    <button @click="g.showDescription = false" class="show-desc">Show less...</button>
+                </template>
+                <template v-else>
+                    <button @click="g.showDescription = true" class="show-desc">Show more...</button>
+                </template>
+                
+            </div>
+        </div>
+    </div>
+
+  
+</template>
+
+<script>
+export default {
+  name: 'ImageGallery',
+  
+  data() {
+      return {
+          gallery: [
+              { imgSrc: '../assets/img_5terre.jpg', imgAlt: 'Cinque Terre', showDescription: false, description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' },
+              { imgSrc: '../assets/img_forest.jpg', imgAlt: 'Forrest', showDescription: false, description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' },
+              { imgSrc: '../assets/img_lights.jpg', imgAlt: 'Lights', showDescription: false, description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' },
+              { imgSrc: '../assets/img_mountains.jpg', imgAlt: 'Mountains', showDescription: false, description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' }
+          ]
+         
+          }
+
+      }
+  }
+}
+
+</script>>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass"  scoped>
+
+.gallery 
+  margin: 5px
+  border: 1px solid #ccc
+  float: left
+  width: 180px
+
+  .desc
+    padding: 15px
+    text-align: center
+
+    .show-desc
+        background-color: #008cba
+        border: none
+        color: white
+        padding: 10px 20px
+        text-align: center
+        text-decoration: none
+        display: inline-block
+
+            
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/components/Home'
 // import Page1 from '@/components/Page1'
 import ComputedProperty from '@/components/ComputedProperty'
 import Methods from '@/components/Methods'
+import ImageGallery from '@/components/ImageGallery'
 
 Vue.use(Router)
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/methods',
       name: 'Methods',
       component: Methods
+    },
+    {
+      path: '/image-gallery',
+      name: 'ImageGallery',
+      component: ImageGallery
     }
   ]
 })
